a2cbda712d37205c44aec031c8fb346da7e59f52
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-04-18 00:37:52 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,4 @@
 
-/*Conexión a la Api*/
-// Definir variables para el paginador
-var elementosPorPagina = 10;
-var currentPage = 1;
-var totalPages = 0;
-
-// Función para mostrar los elementos de una página específica
-function showPage(page) {
-  // Calcular los índices de los elementos a mostrar
-  var startIndex = (page - 1) * elementosPorPagina;
-  var endIndex = startIndex + elementosPorPagina;
-
-  // Ocultar todas las filas
-  $("table tbody tr").hide();
-
-  // Mostrar solo las filas de la página actual
-  $("table tbody tr").slice(startIndex, endIndex).show();
-
-  // Actualizar el número de página actual
-  currentPage = page;
-
-  // Actualizar los botones de navegación
-  updatePagination();
-}
-
-// Función para actualizar los botones de navegación
-function updatePagination() {
-  // Calcular el número total de páginas
-  totalPages = Math.ceil($("table tbody tr").length / elementosPorPagina);
-
-  // Ocultar o mostrar los botones según sea necesario
-  if (currentPage === 1) {
-    $("#previous-page-btn").prop("disabled", true);
-  } else {
-    $("#previous-page-btn").prop("disabled", false);
-  }
-
-  if (currentPage === totalPages) {
-    $("#next-page-btn").prop("disabled", true);
-  } else {
-    $("#next-page-btn").prop("disabled", false);
-  }
-
-  // Actualizar el texto con el número de página actual y el número total de páginas
-  $("#current-page-text").text("Página " + currentPage + " de " + totalPages);
-}
-
 // Conexión a la API
 $.ajax({
   type: "GET",
@@ -55,35 +8,18 @@ $.ajax({
     $.each(response, function (i, data) {
       $("tbody").append(`
             <tr>
-            <th scope="row">${i + 1}</th>
-            <td>${data.name}</td>
-            <td>${data.level}</td>
-            <td><img src="${data.img}" alt="${data.name}"></td>
+            <th scope="row"><p id="numero2">${i + 1}</p></th>
+            <td id="dname"> <p id="dname2">${data.name}</p></td>
+            <td id="dlevel"><p id="dlevel2">${data.level}</p></td>
+            <td id="dimg"><img id="dimg2" src="${data.img}" alt="${data.name}"></td>
             </tr>
             `);
     });
-
-    // Mostrar la primera página
-    showPage(1);
-  },
+    },
+    error: function (error) {
+    console.log(error);
+    },
 });
-
-// Añadir los botones de navegación al DOM
-$("#pagination").append(`
-  <button id="previous-page-btn" class="btn btn-primary" disabled>Anterior</button>
-  <span id="current-page-text"></span>
-  <button id="next-page-btn" class="btn btn-primary">Siguiente</button>
-`);
-
-// Añadir los eventos para los botones de navegación
-$("#previous-page-btn").click(function () {
-  showPage(currentPage - 1);
-});
-
-$("#next-page-btn").click(function () {
-  showPage(currentPage + 1);
-});
-
 
 //Barra de busqueda
 $("#barraBusqueda").on("keyup", function () {
@@ -104,16 +40,3 @@ if (numResultados === 0) {
     $("table tbody tr:last-child").remove();
 }
 });
-
-
-
-
-
-
-
-/*<tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>*/
